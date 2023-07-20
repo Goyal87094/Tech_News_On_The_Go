62f6838c5f74ddff1dda50de58c80d52aac65da4
Goyal87094 <111359110+Goyal87094@users.noreply.github.com>
2023-07-20 20:15:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Search from "./search";
+import Pagination from "./pagination";
+import Stories from "./stories";
+import "./App.css";
+
+const App = () => {
+  
+  return(
+    <>
+    
+      <Search />
+      <Pagination />
+      <Stories />
+    </>
+  );
+};
+
+export default App;
